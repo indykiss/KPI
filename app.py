ab8f29e28906911a36ae528792ccb3c60c4cee31
indykiss <indirak@alumni.princeton.edu>
2021-09-26 23:17:47 -0400

    add submit button

--- app.py
+++ app.py
@@ -114,6 +114,9 @@ app.layout = html.Div([
 		multi=True
 	), 
 
+	# Add a submit button
+	html.Button('Submit', id='input-submit', n_clicks=0),
+
 	# Next step: Just add a graph or any info that displays 
 	# the client's ticker info
 
@@ -121,7 +124,8 @@ app.layout = html.Div([
 	html.Div(id='output-date-picker'),
 	html.Div(id='output-client-picker'),
 	html.Div(id='output-date-range-picker'),
-	html.Div(id='output-subindustry-picker'), 
+	html.Div(id='output-subindustry-picker'),
+	html.Div(id='output-submit'), 
 	html.Div(id='output-yf-client-ticker')	
 ])
 
@@ -226,11 +230,12 @@ def update_client_picker(ticker):
 	Input('input-date-range-picker', 'start_date'), 
 	Input('input-date-range-picker', 'end_date'),
 	Input('input-date-picker', 'date'),
+	Input('input-submit', 'n_clicks'), 
 	Input('input-client-picker', 'value')])
 
 # Need to do expected input/ output audit for these functions for tests
-def update_output(companies, start_date, end_date, launch_date, client):
-	if companies is None or start_date is None or end_date is None or launch_date is None or client is None:
+def update_output(companies, start_date, end_date, launch_date, submitted, client):
+	if submitted < 1:
 		raise PreventUpdate
 
 	all_companies = [*companies]
@@ -256,6 +261,7 @@ def update_output(companies, start_date, end_date, launch_date, client):
 	return make_graph(dates_data, all_growths, client_growth)
 
 
+# FIX SECOND TRACE LINE NOT WORKING
 # Second trace line not visible for some reason?
 def make_graph(dates, all_growths, client_growth):
 	fig = go.Figure()
@@ -281,303 +287,25 @@ def make_graph(dates, all_growths, client_growth):
 
 	return newGraph
 
-	# fig = go.Figure(
-	# 	data=[go.Scatter(x=dates, y=all_growths)],
-	# 	layout=go.Layout(
-	# 		title=go.layout.Title(text="Custom index growth over time"))
-	# )
-
-	# fig.add_trace(go.Scatter(
-	# 						x=dates,
-	# 						y=client_growth,
-	# 						mode="lines",
-	# 						line=go.scatter.Line(color="gray")
-	# 						# showlegend=False
-	# 						))
-
-	# newGraph = html.Div(dcc.Graph(
-	# 	id='output-subindustry-picker',
-	# 	figure=fig
-	# ))
-
-	# return newGraph    
 
 
+# Hit submit button to load graph
+@app.callback(
+    Output('output-submit', 'children'),
+	[Input('input-submit', 'n_clicks'), 
+	Input('input-subindustry-picker', 'value'), 
+	Input('input-date-range-picker', 'start_date'), 
+	Input('input-date-range-picker', 'end_date'),
+	Input('input-date-picker', 'date'),
+	Input('input-client-picker', 'value')])
+
+def update_output(n_clicks, companies, start_date, end_date, launch_date, client):
+	if companies is None or start_date is None or end_date is None or launch_date is None or client is None:
+		raise PreventUpdate
+	else:
+		return 'Please be patient. Graph loading times vary.'
 
 
-
-# These next 3 functions are perfect and work VERY well
-# I'm just commenting out so I can try the new versions of 
-# these functions, so we can add client growth trace
-# Would be good to divide into a couple functions
-
-# def update_output(companies, start_date, end_date, client, launch_date):	
-# 	dict = {} # for debugging company : stock changes
-# 	client_growth = []
-
-# 	if start_date is not None:
-# 		growths_in_arrs = [] # for slightly easier averaging
-# 		dates_data = pd.date_range(start_date, end_date, periods=6)
-# 	#  end_date_object = date.fromisoformat(end_date)
-
-# 	for x in companies: 
-# 		if start_date is not None:			
-# 			# start_date_object = date.fromisoformat(start_date)
-# 			# new_date = start_date_object + timedelta(days=30)
-
-# 			df = pdr.get_data_yahoo(x, start_date, end_date)
-# 			stock_data = df['Open']
-# 			prev_day = 0
-# 			stock_change = []	
-
-# 			print(stock_data)			
-
-# 			# Calculates growth over time for each company 
-# 			for day_price in stock_data:
-# 				if prev_day == 0:
-# 				    prev_day = day_price
-# 				else:
-# 					percent_change = ((day_price - prev_day) / day_price) * 100 
-# 					rounded = round(percent_change, 3)
-# 					stock_change.append(rounded)
-# 					if x == client:
-# 						client_growth.append(rounded)
-# 					prev_day = day_price
-
-# 			# after finishing creating an array of % changes from start to end
-# 			dict[x] = stock_change
-# 			growths_in_arrs.append(stock_change)
-
-# 	if start_date is not None:
-# 		return average_stock_growths(dates_data, growths_in_arrs, client_growth)
-
-# def average_stock_growths(dates, growths_in_arrs, client_growth):
-# 	all_average_growths = []
-# 	num_days = len(growths_in_arrs[0])
-# 	i = 0
-
-# 	#  list index out of range error here sometimes
-# 	while i < num_days:
-# 		sum = 0
-# 		for arr in growths_in_arrs:
-# 			sum += arr[i]
-# 		i += 1
-
-# 		avg = sum / len(growths_in_arrs)
-# 		rounded = round(avg, 3)
-# 		all_average_growths.append(rounded)
-# 	return make_graph(dates, all_average_growths, client_growth)
-
-# def make_graph(dates, avg_growth, client_growth):
-	# fig = go.Figure(
-	# 	data=[go.Scatter(x=dates, y=avg_growth)],
-	# 	layout=go.Layout(
-	# 		title=go.layout.Title(text="Custom index growth over time"))
-	# )
-
-	# newGraph = html.Div(dcc.Graph(
-	# 	id='output-subindustry-picker',
-	# 	figure=fig
-	# ))
-	# reference_line2 = go.Scatter(x=dates,
-	# 						y=client_growth,
-	# 						# showlegend=False
-	# 						)
-	# fig.add_trace(reference_line2)
-
-	# return newGraph
-
-
-
-
-
-# Add a trace line over graph, different color 
-
-
-
-						##### NOTES ####
-# INDUSTRY GROWTH --> LOOK AT % STOCK PRICE GROWTH MONTH OVER MONTH FOR 1 YEAR PRE-LAUNCH
-# AND 1 YEAR POST LAUNCH. USE GROCERY STORE SUBINDUSTRY AS EXAMPLE 
-
-# MARKET CAPITALIZATION WEIGHTED IS ANOTHER WAY TO MAKE THE GRAPH
-# MAYBE AN EXTENDED FEATURE
-
-# ANOTHER STEP IS LOOKING AT HOW TO BUILD A GRAPH WITH THE DATA 
-# https://medium.com/swlh/how-to-create-a-dashboard-to-dominate-the-stock-market-using-python-and-dash-c35a12108c93
-# DOES A GREAT JOB OF EXPLAINING WHAT TO DO 
-
-
-
-
-
-# IDK JUST OLDER STUFF THAT I'M NOT CURRENTLY USING. 
-# DELETE THIS SOON PROBABLY 
-
-# I couldn't get output working bc of some antivirus or version thing, so dropping jupyter right now. 
-# 
-# https://mybinder.org/v2/gh/ipython/ipython-in-depth/7e5ce96cc9251083979efdfc393425f1229a4a68?filepath=binder%2FIndex.ipynb
-
-# def update_output(companies, start_date, end_date):
-# 	#print('You have selected "{}"'.format(companies))
-	
-# 	hash = {}
-# 	#  end_date_object = date.fromisoformat(end_date)
-
-# 	for x in companies: 
-# 		if start_date is not None:			
-			# start_date_object = date.fromisoformat(start_date)
-			# new_date = start_date_object + timedelta(days=30)
-			# NEED TO PULL YFINANCE DATA FOR EACH NEW_DATE
-			# MAYBE USE PANDAS TO STRUCURE DATA LIKE EXCEL
-
-			# IDK MAYBE TRYING TO REPLACE BELOW WITH PANDAS
-			# while new_date < end_date_object:
-			# 	temp = x.upper()
-			# 	# Use the new date to access the correct data
-			# 	TICKER = yf.Ticker(temp)
-			# 	# print(TICKER.history(period = new_date)["open"])
-
-			# 	price = TICKER.info['open']
-			# 	prices.append(price)
-			# 	new_date = new_date + timedelta(30)	
-			# hash[x] = prices
-
-		# This while loop isn't great. The yfinance stuff is accurate
-		# just need to get the right dates
-		# while begin < end_date:
-		# 	temp = x.upper()
-		# 	TICKER = yf.Ticker(temp)
-		# 	price = TICKER.info['open']
-		# 	prices.append(price)
-		# 	begin = begin + datetime.timedelta(days=30) # 30 days?
-		# 	print(begin)
-
-
-
-# def get_info_box(): 
-# 	return html.Div([
-# 			html.Div([dcc.Dropdown(id='stock-index-choice', #???
-# 				                   options=index_choice,
-# 				                   value=index_choice[0]['value']
-# 								   )]),
-# 			html.Div(style={'width':'20%','display': 'inline-block'}),
-# 			html.Div([dcc.Dropdown(id='stock-include',
-# 				                   options=[],
-# 				                   value=[],
-# 				                   multi=True
-# 				                )])
-# 		])    
-
-
-
-# unused at the moment: stock ticker lookup 
-def verify_ticker(ticker):
-	tick = re.findall('^[A-Za-z]{1,4}$', ticker) 
-	# python regex to find matches and return strings 
-	if len(tick)>0:
-		return True, tick[0].upper()
-	else: 
-		return False
-
-# ticker = "AAPL"
-
-def get_ticker(n_clicks, time, ticker, mkt):
-	# For default setting
-	if ticker == '':
-		return 'Please Enter a Stock Ticker', \
-			   '','','',{'width':'20%', 'display':'inline-block'},'', \
-			   {'width':'20%', 'display':'inline-block'},'', \
-			   {'data':None}, None
-	# Verify ticker format in respective to stock market
-	stockFormat, ticker = verify_ticker(ticker, mkt)
-	# Catch incorrect 
-	if stockFormat is False:
-		return 'Wrong Ticker', '#######', '$##.##', '##.##', \
-			   {'width':'20%', 'display':'inline-block'}, '##.##%', \
-			   {'width':'20%', 'display':'inline-block'}, \
-			   'Error! Please try again.', {'data':None}, None
-	# Obtain stock price and stats
-	stock = yfinance.Ticker(ticker)
-	# Catch if stock exists
-	if stock.history(period='ytd').shape[0] == 0:
-		return 'Wrong Ticker', '#######', '$##.##', '##.##', \
-			   {'width':'20%', 'display':'inline-block'}, '##.##%', \
-			   {'width':'20%', 'display':'inline-block'}, \
-			   'Error! Please try again.', {'data':None}, None
-	### Stock Stats for Info Box ###
-	try: 
-		# Name and price
-		stock_name = stock.info['longName']
-		price_list = stock.history(period=time)['Close'].tolist()
-		price = f'${price_list[-1]:.2f}'
-		# Price Change
-		price_change = price_list[-1] - price_list[-2]
-		price_percent_change = (price_list[-1]/price_list[-2])-1
-		if price_change > 0:
-			price_change_colour = {'color':'green'}
-		else:
-			price_change_colour = {'color':'red'}
-		price_change_colour['display']= 'inline-block'
-		price_change_colour['width']= '20%'
-		price_change_colour['font-size'] = '150%'
-		price_change = f'{price_change:.2f}'
-		price_percent_change = f'{price_percent_change*100:,.2f}%'
-
-		df = getMA(stock, time, 
-				   stock.history(period=time).reset_index()['Date'])
-
-		fig = getCandlestick(df)
-		table = getTab1Table(stock.history(period=time).reset_index(),
-							 stock.info)
-
-	except:
-		return 'Sorry! Company Not Available', '#######', '$##.##', '##.##', \
-			   {'width':'20%', 'display':'inline-block'}, '##.##%', \
-			   {'width':'20%', 'display':'inline-block'}, \
-			   'Error! Please try again another Company.', {'data':None}, None
-
-
-	return stock_name, ticker, price, price_change, price_change_colour, \
-		   price_percent_change, price_change_colour, '', fig, table
-
-
-
-def get_tab1_info_box():
-	return html.Div([
-				html.Div([
-					html.Div(id='tab1-stock-price',
-							 style={'width':'30%','display':'inline-block',
-									'font-size':'200%'}),
-					html.Div(id='tab1-stock-price-change'),
-					html.Div(id='tab1-stock-price-percentchange')
-					],style={'width':'30%','display': 'inline-block',
-							 'vertical-align':'top'}),
-				html.Div(style={'width':'15%','display': 'inline-block'}),
-				html.Div(dcc.Dropdown(id='tab1-market-dropdown',
-									  options=tab1_markets,
-									  value=tab1_markets[0]['value'],
-									  style={'text-align':'left'}),
-						 style={'width':'20%','display': 'inline-block',
-								'vertical-align':'top'}),
-				html.Div(style={'width':'5%','display': 'inline-block'}),
-				html.Div([
-					html.Div([
-						html.Div(dcc.Input(id='tab1-ticker-input',value='',
-										   type='text'),
-										   style={'display': 'inline-block'}),
-						html.Div(html.Button('Submit',id='tab1-submit'),
-								 style={'display': 'inline-block'})
-						]),
-					html.Div(id='tab1-error-message', style={'color':'red'}, children='Error Box')
-					],style={'width':'30%','display': 'inline-block'})
-			])
-
-
-# def get_ticker():
-#     stock = yfinance.Ticker(ticker)
-
-#     print(stock)
-# #     Assume stock ticker exists, but need to add check if doesnt
 
 
 if __name__ == '__main__':
--- worksheet.py
+++ worksheet.py
@@ -139,3 +139,327 @@ def make_graph(dates, all_growths, client_growth):
 
 
 
+
+
+# Functions that are okay and want to clear off my app.py
+
+
+# These next 3 functions are perfect and work VERY well
+# I'm just commenting out so I can try the new versions of 
+# these functions, so we can add client growth trace
+# Would be good to divide into a couple functions
+
+# def update_output(companies, start_date, end_date, client, launch_date):	
+# 	dict = {} # for debugging company : stock changes
+# 	client_growth = []
+
+# 	if start_date is not None:
+# 		growths_in_arrs = [] # for slightly easier averaging
+# 		dates_data = pd.date_range(start_date, end_date, periods=6)
+# 	#  end_date_object = date.fromisoformat(end_date)
+
+# 	for x in companies: 
+# 		if start_date is not None:			
+# 			# start_date_object = date.fromisoformat(start_date)
+# 			# new_date = start_date_object + timedelta(days=30)
+
+# 			df = pdr.get_data_yahoo(x, start_date, end_date)
+# 			stock_data = df['Open']
+# 			prev_day = 0
+# 			stock_change = []	
+
+# 			print(stock_data)			
+
+# 			# Calculates growth over time for each company 
+# 			for day_price in stock_data:
+# 				if prev_day == 0:
+# 				    prev_day = day_price
+# 				else:
+# 					percent_change = ((day_price - prev_day) / day_price) * 100 
+# 					rounded = round(percent_change, 3)
+# 					stock_change.append(rounded)
+# 					if x == client:
+# 						client_growth.append(rounded)
+# 					prev_day = day_price
+
+# 			# after finishing creating an array of % changes from start to end
+# 			dict[x] = stock_change
+# 			growths_in_arrs.append(stock_change)
+
+# 	if start_date is not None:
+# 		return average_stock_growths(dates_data, growths_in_arrs, client_growth)
+
+# def average_stock_growths(dates, growths_in_arrs, client_growth):
+# 	all_average_growths = []
+# 	num_days = len(growths_in_arrs[0])
+# 	i = 0
+
+# 	#  list index out of range error here sometimes
+# 	while i < num_days:
+# 		sum = 0
+# 		for arr in growths_in_arrs:
+# 			sum += arr[i]
+# 		i += 1
+
+# 		avg = sum / len(growths_in_arrs)
+# 		rounded = round(avg, 3)
+# 		all_average_growths.append(rounded)
+# 	return make_graph(dates, all_average_growths, client_growth)
+
+# def make_graph(dates, avg_growth, client_growth):
+	# fig = go.Figure(
+	# 	data=[go.Scatter(x=dates, y=avg_growth)],
+	# 	layout=go.Layout(
+	# 		title=go.layout.Title(text="Custom index growth over time"))
+	# )
+
+	# newGraph = html.Div(dcc.Graph(
+	# 	id='output-subindustry-picker',
+	# 	figure=fig
+	# ))
+	# reference_line2 = go.Scatter(x=dates,
+	# 						y=client_growth,
+	# 						# showlegend=False
+	# 						)
+	# fig.add_trace(reference_line2)
+
+	# return newGraph
+
+
+
+
+
+# Add a trace line over graph, different color 
+
+
+
+						##### NOTES ####
+# INDUSTRY GROWTH --> LOOK AT % STOCK PRICE GROWTH MONTH OVER MONTH FOR 1 YEAR PRE-LAUNCH
+# AND 1 YEAR POST LAUNCH. USE GROCERY STORE SUBINDUSTRY AS EXAMPLE 
+
+# MARKET CAPITALIZATION WEIGHTED IS ANOTHER WAY TO MAKE THE GRAPH
+# MAYBE AN EXTENDED FEATURE
+
+# ANOTHER STEP IS LOOKING AT HOW TO BUILD A GRAPH WITH THE DATA 
+# https://medium.com/swlh/how-to-create-a-dashboard-to-dominate-the-stock-market-using-python-and-dash-c35a12108c93
+# DOES A GREAT JOB OF EXPLAINING WHAT TO DO 
+
+
+
+
+
+# IDK JUST OLDER STUFF THAT I'M NOT CURRENTLY USING. 
+# DELETE THIS SOON PROBABLY 
+
+# I couldn't get output working bc of some antivirus or version thing, so dropping jupyter right now. 
+# 
+# https://mybinder.org/v2/gh/ipython/ipython-in-depth/7e5ce96cc9251083979efdfc393425f1229a4a68?filepath=binder%2FIndex.ipynb
+
+# def update_output(companies, start_date, end_date):
+# 	#print('You have selected "{}"'.format(companies))
+	
+# 	hash = {}
+# 	#  end_date_object = date.fromisoformat(end_date)
+
+# 	for x in companies: 
+# 		if start_date is not None:			
+			# start_date_object = date.fromisoformat(start_date)
+			# new_date = start_date_object + timedelta(days=30)
+			# NEED TO PULL YFINANCE DATA FOR EACH NEW_DATE
+			# MAYBE USE PANDAS TO STRUCURE DATA LIKE EXCEL
+
+			# IDK MAYBE TRYING TO REPLACE BELOW WITH PANDAS
+			# while new_date < end_date_object:
+			# 	temp = x.upper()
+			# 	# Use the new date to access the correct data
+			# 	TICKER = yf.Ticker(temp)
+			# 	# print(TICKER.history(period = new_date)["open"])
+
+			# 	price = TICKER.info['open']
+			# 	prices.append(price)
+			# 	new_date = new_date + timedelta(30)	
+			# hash[x] = prices
+
+		# This while loop isn't great. The yfinance stuff is accurate
+		# just need to get the right dates
+		# while begin < end_date:
+		# 	temp = x.upper()
+		# 	TICKER = yf.Ticker(temp)
+		# 	price = TICKER.info['open']
+		# 	prices.append(price)
+		# 	begin = begin + datetime.timedelta(days=30) # 30 days?
+		# 	print(begin)
+
+
+
+# def get_info_box(): 
+# 	return html.Div([
+# 			html.Div([dcc.Dropdown(id='stock-index-choice', #???
+# 				                   options=index_choice,
+# 				                   value=index_choice[0]['value']
+# 								   )]),
+# 			html.Div(style={'width':'20%','display': 'inline-block'}),
+# 			html.Div([dcc.Dropdown(id='stock-include',
+# 				                   options=[],
+# 				                   value=[],
+# 				                   multi=True
+# 				                )])
+# 		])    
+
+
+
+# unused at the moment: stock ticker lookup 
+def verify_ticker(ticker):
+	tick = re.findall('^[A-Za-z]{1,4}$', ticker) 
+	# python regex to find matches and return strings 
+	if len(tick)>0:
+		return True, tick[0].upper()
+	else: 
+		return False
+
+# ticker = "AAPL"
+
+def get_ticker(n_clicks, time, ticker, mkt):
+	# For default setting
+	if ticker == '':
+		return 'Please Enter a Stock Ticker', \
+			   '','','',{'width':'20%', 'display':'inline-block'},'', \
+			   {'width':'20%', 'display':'inline-block'},'', \
+			   {'data':None}, None
+	# Verify ticker format in respective to stock market
+	stockFormat, ticker = verify_ticker(ticker, mkt)
+	# Catch incorrect 
+	if stockFormat is False:
+		return 'Wrong Ticker', '#######', '$##.##', '##.##', \
+			   {'width':'20%', 'display':'inline-block'}, '##.##%', \
+			   {'width':'20%', 'display':'inline-block'}, \
+			   'Error! Please try again.', {'data':None}, None
+	# Obtain stock price and stats
+	stock = yfinance.Ticker(ticker)
+	# Catch if stock exists
+	if stock.history(period='ytd').shape[0] == 0:
+		return 'Wrong Ticker', '#######', '$##.##', '##.##', \
+			   {'width':'20%', 'display':'inline-block'}, '##.##%', \
+			   {'width':'20%', 'display':'inline-block'}, \
+			   'Error! Please try again.', {'data':None}, None
+	### Stock Stats for Info Box ###
+	try: 
+		# Name and price
+		stock_name = stock.info['longName']
+		price_list = stock.history(period=time)['Close'].tolist()
+		price = f'${price_list[-1]:.2f}'
+		# Price Change
+		price_change = price_list[-1] - price_list[-2]
+		price_percent_change = (price_list[-1]/price_list[-2])-1
+		if price_change > 0:
+			price_change_colour = {'color':'green'}
+		else:
+			price_change_colour = {'color':'red'}
+		price_change_colour['display']= 'inline-block'
+		price_change_colour['width']= '20%'
+		price_change_colour['font-size'] = '150%'
+		price_change = f'{price_change:.2f}'
+		price_percent_change = f'{price_percent_change*100:,.2f}%'
+
+		df = getMA(stock, time, 
+				   stock.history(period=time).reset_index()['Date'])
+
+		fig = getCandlestick(df)
+		table = getTab1Table(stock.history(period=time).reset_index(),
+							 stock.info)
+
+	except:
+		return 'Sorry! Company Not Available', '#######', '$##.##', '##.##', \
+			   {'width':'20%', 'display':'inline-block'}, '##.##%', \
+			   {'width':'20%', 'display':'inline-block'}, \
+			   'Error! Please try again another Company.', {'data':None}, None
+
+
+	return stock_name, ticker, price, price_change, price_change_colour, \
+		   price_percent_change, price_change_colour, '', fig, table
+
+
+
+def get_tab1_info_box():
+	return html.Div([
+				html.Div([
+					html.Div(id='tab1-stock-price',
+							 style={'width':'30%','display':'inline-block',
+									'font-size':'200%'}),
+					html.Div(id='tab1-stock-price-change'),
+					html.Div(id='tab1-stock-price-percentchange')
+					],style={'width':'30%','display': 'inline-block',
+							 'vertical-align':'top'}),
+				html.Div(style={'width':'15%','display': 'inline-block'}),
+				html.Div(dcc.Dropdown(id='tab1-market-dropdown',
+									  options=tab1_markets,
+									  value=tab1_markets[0]['value'],
+									  style={'text-align':'left'}),
+						 style={'width':'20%','display': 'inline-block',
+								'vertical-align':'top'}),
+				html.Div(style={'width':'5%','display': 'inline-block'}),
+				html.Div([
+					html.Div([
+						html.Div(dcc.Input(id='tab1-ticker-input',value='',
+										   type='text'),
+										   style={'display': 'inline-block'}),
+						html.Div(html.Button('Submit',id='tab1-submit'),
+								 style={'display': 'inline-block'})
+						]),
+					html.Div(id='tab1-error-message', style={'color':'red'}, children='Error Box')
+					],style={'width':'30%','display': 'inline-block'})
+			])
+
+
+# def get_ticker():
+#     stock = yfinance.Ticker(ticker)
+
+#     print(stock)
+# #     Assume stock ticker exists, but need to add check if doesnt
+
+
+
+def make_graph(dates, all_growths, client_growth):
+	fig = go.Figure()
+
+	fig.add_trace(go.Scatter(
+		x=dates,
+		y=all_growths, 
+		mode="lines+markers",
+		name="Custom index growth"
+	))
+
+	fig.add_trace(go.Scatter(
+		x=dates,
+		y=client_growth,
+		mode="lines+markers",
+		name="Client growth"
+	))
+
+	newGraph = html.Div(dcc.Graph(
+		id='output-subindustry-picker',
+		figure=fig
+	))	
+
+	return newGraph
+
+	# fig = go.Figure(
+	# 	data=[go.Scatter(x=dates, y=all_growths)],
+	# 	layout=go.Layout(
+	# 		title=go.layout.Title(text="Custom index growth over time"))
+	# )
+
+	# fig.add_trace(go.Scatter(
+	# 						x=dates,
+	# 						y=client_growth,
+	# 						mode="lines",
+	# 						line=go.scatter.Line(color="gray")
+	# 						# showlegend=False
+	# 						))
+
+	# newGraph = html.Div(dcc.Graph(
+	# 	id='output-subindustry-picker',
+	# 	figure=fig
+	# ))
+
+	# return newGraph    
