b70bb14e26cb28b55515bdb4cf9edaf6e0932565
indykiss <indirak@alumni.princeton.edu>
2021-12-05 13:31:48 -0500

    debug dash importerror

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import dash
 from dash import dcc
 from dash import html
 
-import dash_bootstrap_components as dbc
+# import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output # So I can simplify callback
 from dash.exceptions import PreventUpdate
 
